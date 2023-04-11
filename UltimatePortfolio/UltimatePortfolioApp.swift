c6c1fc8ad0777dd7cae1574ca75e1cbc29e8979a
Uhl Albert <bikrrr+git@gmail.com>
2023-04-11 15:05:57 -0600

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Uhl Albert on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
