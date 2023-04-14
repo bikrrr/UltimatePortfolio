6b88fd8ebce570b5cf3b9651a4e3991f077ad0a6
Uhl Albert <bikrrr+git@gmail.com>
2023-04-14 16:27:51 -0600

    End of "Part 1" section.

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -7,6 +7,15 @@
 
 import CoreData
 
+enum SortType: String {
+    case dateCreated = "creationDate"
+    case dateModified = "modificationDate"
+}
+
+enum Status {
+    case all, open, closed
+}
+
 class DataController: ObservableObject {
     let container: NSPersistentCloudKitContainer
     
@@ -15,6 +24,12 @@ class DataController: ObservableObject {
     
     @Published var filterText = ""
     @Published var filterTokens = [Tag]()
+    
+    @Published var filterEnabled = false
+    @Published var filterPriority = -1
+    @Published var filterStatus = Status.all
+    @Published var sortType = SortType.dateCreated
+    @Published var sortNewestFirst = true
     
     private var saveTask: Task<Void, Error>?
 
@@ -166,6 +181,19 @@ class DataController: ObservableObject {
             }
         }
         
+        if filterEnabled {
+            if filterPriority >= 0 {
+                let priorityFilter = NSPredicate(format: "priority = %d", filterPriority)
+                predicates.append(priorityFilter)
+            }
+            
+            if filterStatus != .all {
+                let lookForClosed = filterStatus == .closed
+                let statusFilter = NSPredicate(format: "completed = %@", NSNumber(value: lookForClosed))
+                predicates.append(statusFilter)
+            }
+        }
+        
         // search tags with OR operator
 //        if filterTokens.isEmpty == false {
 //            let tokenPredicate = NSPredicate(format: "ANY tags in %@", filterTokens)
@@ -174,9 +202,8 @@ class DataController: ObservableObject {
         
         let request = Issue.fetchRequest()
         request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: predicates)
-        
+        request.sortDescriptors = [NSSortDescriptor(key: sortType.rawValue, ascending: sortNewestFirst)]
         let allIssues = (try? container.viewContext.fetch(request)) ?? []
-
         return allIssues.sorted()
     }
 }
