cb4377979f9903cedc9660eb382bfc958a8ed032
Uhl Albert <bikrrr+git@gmail.com>
2023-04-13 18:32:25 -0600

    End of "5. Cleaning up Core Data" video

---
+++ UltimatePortfolio/Issue-CoreDataHelpers.swift
@@ -0,0 +1,58 @@
+//
+//  Issue-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Uhl Albert on 4/13/23.
+//
+
+import Foundation
+
+extension Issue {
+    var issueTitle: String {
+        get { title ?? "" }
+        set { title = newValue }
+    }
+    
+    var issueContent: String {
+        get { content ?? "" }
+        set { content = newValue }
+    }
+    
+    var issueCreationDate: Date {
+        creationDate ?? .now
+    }
+    
+    var issueModificationDate: Date {
+        modificationDate ?? .now
+    }
+    
+    var issueTags: [Tag] {
+        let result = tags?.allObjects as? [Tag] ?? []
+        return result.sorted()
+    }
+    
+    static var example: Issue {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let issue = Issue(context: viewContext)
+        issue.title = "Example Issue"
+        issue.content = "This is an example issue."
+        issue.priority = 2
+        issue.creationDate = .now
+        return issue
+    }
+}
+
+extension Issue: Comparable {
+    public static func <(lhs: Issue, rhs: Issue) -> Bool {
+        let left = lhs.issueTitle.localizedLowercase
+        let right = rhs.issueTitle.localizedLowercase
+        
+        if left == right {
+            return lhs.issueCreationDate < rhs.issueCreationDate
+        } else {
+            return left < right
+        }
+    }
+}
---
+++ UltimatePortfolio/tag-CoreDataHelpers.swift
@@ -0,0 +1,45 @@
+//
+//  tag-CoreDataHelpers.swift
+//  UltimatePortfolio
+//
+//  Created by Uhl Albert on 4/13/23.
+//
+
+import Foundation
+
+extension Tag {
+    var tagID: UUID {
+        id ?? UUID()
+    }
+    
+    var tagName: String {
+        name ?? ""
+    }
+    
+    var tagActiveIssues: [Issue] {
+        let result = issues?.allObjects as? [Issue] ?? []
+        return result.filter { $0.completed == false }
+    }
+    
+    static var example: Tag {
+        let controller = DataController(inMemory: true)
+        let viewContext = controller.container.viewContext
+        
+        let tag = Tag(context: viewContext)
+        tag.id = UUID()
+        tag.name = "Example Tag"
+        return tag
+    }
+}
+
+extension Tag: Comparable {
+    public static func <(lhs: Tag, rhs: Tag) -> Bool {
+        let left = lhs.tagName.localizedLowercase
+        let right = rhs.tagName.localizedLowercase
+        if left == right {
+            return lhs.tagID.uuidString < rhs.tagID.uuidString
+        } else {
+            return left < right
+        }
+    }
+}
